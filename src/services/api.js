50fa0060fa2657beb07c02e1f34f662de8c7a22a
Edilson <juniorse13@gmail.com>
2021-09-24 15:17:09 -0300

    upadting fakeApi url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://my-json-server.typicode.com/EdilsonJunior-2/fakeApi",
+  baseURL: "https://my-json-server.typicode.com/EdilsonJunior-2/debt-project",
 });
 
 export const usersApi = axios.create({
